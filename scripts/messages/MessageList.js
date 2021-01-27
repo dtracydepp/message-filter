1ee03d70ea3500db9d199592f77edf41d94c8ad0
dtracydepp <tracydepp82@gmail.com>
2021-01-27 15:05:22 -0600

    refactor friendlist to add a custom event, added event listener to message list to filter message by friend selected.

---
+++ scripts/messages/MessageList.js
@@ -0,0 +1,31 @@
+import { getMessagesByFriend } from "./MessageProvider.js"
+import { Message } from "./Message.js"
+
+const contentTarget = document.querySelector(".messages")
+const eventHub = document.querySelector(".container")
+
+/*
+    COMPONENT FUNCTION
+*/
+export const MessageList = () => {
+    render([])
+}
+
+/*
+    RENDERING FUNCTION
+*/
+const render = messageArray => {
+    const convertedMessages = messageArray.map(messageObject => {
+        const messageHTML = Message(messageObject)
+        return messageHTML
+    })
+    const combinedSections = convertedMessages.join("")
+    contentTarget.innerHTML = combinedSections
+}
+
+// Listen for when a friend is selected
+eventHub.addEventListener("friendSelected", event => {
+    const friendName = event.detail.friend
+    const messages = getMessagesByFriend(friendName)
+    render(messages)
+})
